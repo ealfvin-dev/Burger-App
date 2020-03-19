69769dba8321ec1c4af79e1387e2b046d21ca7b3
Erik Alfvin <erikalfvin@gmail.com>
2020-03-19 17:46:29 -0500

    Created database and seed burgers

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers(name) VALUES("Cheese Burger");
+INSERT INTO burgers(name) VALUES("Bacon Burger");
+INSERT INTO burgers(name) VALUES("Black Bean Burger");
+INSERT INTO burgers(name) VALUES("Impossible Burger");
