cc29c8b79a4e5ab0eee2054b5ad070242658bbf7
Erik Alfvin <erikalfvin@gmail.com>
2020-03-19 18:05:31 -0500

    Wrote skeleton for orm

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function() {
+
+    },
+    insertOne: function() {
+
+    },
+    eatOne: function() {
+
+    }
+};
+
+module.exports = orm;
